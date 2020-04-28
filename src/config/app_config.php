f8deb9890d5f799548cb5f41e114119c6c8c32ae
LordKahra <lordkahra@gmail.com>
2020-04-28 01:18:55 -0400

    Environment Variables

--- src/config/app_config.php
+++ src/config/app_config.php
@@ -14,7 +14,7 @@ define("SITE_HOST", getenv("SITE_HOST_DRFL_VIRTUAL"));
 define("DATABASE_HOST", getenv("DATABASE_HOST"));
 define("DATABASE_USERNAME", getenv("DATABASE_USERNAME"));
 define("DATABASE_PASSWORD", getenv("DATABASE_PASSWORD"));
-define("DATABASE_NAME", "drfl_virtual");
+define("DATABASE_NAME", getenv("DATABASE_NAME"));
 //define("DATABASE_NAME", getenv("DATABASE_NAME_BOFFER_WIKI"));
 // TODO: Determine if mysql access is needed.
 
